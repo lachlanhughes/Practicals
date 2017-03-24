42a971c3611f55aed71c7ec8802b34e7f6c20c79
Lachlan <lachlan.hughes@my.jcu.edu.au>
2017-03-24 10:57:43 +1000

    Added functions to fixedScore program

--- prac3/fixedScore.py
+++ prac3/fixedScore.py
@@ -3,15 +3,21 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(determine_score(score))
 
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
