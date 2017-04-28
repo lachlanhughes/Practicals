df63db64598d2ee5b6df4899fb952f44a7ffd621
lachlanhughes <lachlan.hughes@my.jcu.edu.au>
2017-04-28 14:57:52 +1000

    prac 7

--- prac7/languages.py
+++ prac7/languages.py
@@ -3,14 +3,18 @@ from prac7.programminglanguage import ProgrammingLanguage
 
 def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    languages = ["ruby"]
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    languages.append("python")
     vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    languages.append("vb")
 
     print(ruby)
 
-    #for i in :
-    #    print(i)
+    print("\nThe dynamically typed languages are:")
+    for i in languages:
+        #if i.is_dynamic():
+            print(i)
 
-    print(dir(ProgrammingLanguage))
 
 main()
--- prac7/programminglanguage.py
+++ prac7/programminglanguage.py
@@ -1,7 +1,6 @@
 
 
 class ProgrammingLanguage:
-
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
